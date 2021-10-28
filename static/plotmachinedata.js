8f682f175e60d9370eec52623199369a714e0c35
Samuel Caldwell <samuelcaldwell97@gmail.com>
2021-10-28 13:48:52 -0500

    updating prediction page

--- static/plotmachinedata.js
+++ static/plotmachinedata.js
@@ -10,39 +10,38 @@ function addPlot() {
         var date_trace = data[0]
         var pm25_trace = data[1]
 
-
-        var traces =  [{
-          x: date_trace,
-          y: pm25_trace,
-          mode: 'lines+markers',
-            name: 'World',
-            fill: 'toself'
-        }];
-
-
-        var layout = {
-            xaxis:{
-                title : {
-                    text: 'Year'
-                }
+    var config = {responsive: true}
+    
+    var data = [
+      {
+        x: date_trace,
+        y: pm25_trace,
+        type: 'line',
+          opacity: 0.6,
+          marker: {
+            color: '#4a934a'
+            }
+      }
+    ];
+    
+    
+    var layout = {
+      title: 'CO2 and GHG Emissions by Country 2016 (Metric Tons)',
+      yaxis: {
+          title: {
+              text: 'Emissions (Metric Tons)'
+          },
             },
-          yaxis: {
-              title: {
-                  text: 'µg/m3'
-              },
-                tickmode: "linear", //  If "linear", the placement of the ticks is determined by a starting position `tick0` and a tick step `dtick`
-                tick0: 0,
-                dtick: 20,
-                range: [-3, 110]
-                },
-            plot_bgcolor:"#f8f9fa",
-            paper_bgcolor:"#f8f9fa",
-            autosize: true, 
-            height:550,
-            colorway : ['#4a934a', '#488A99', '#1F3F49', '#4CB5F5', '#23282D', '#9fe2bf', '#7E909A', '#A5D8DD']
-          }
+        plot_bgcolor:"#f8f9fa",
+        paper_bgcolor:"#f8f9fa",
+        autosize: true, 
+        height:550,
+        bargap:0.05
+      }
+    
+    
 
-        Plotly.newPlot('predictions_plot', traces, layout, config);
-        }
-                                 );
+    Plotly.newPlot('predictions_plot', data, layout, config);
+    
+    });
 };
